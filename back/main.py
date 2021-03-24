3707ff562cade25c364e7db31343510b31e9216d
Mauricio Neira <m.neira10@uniandes.edu.co>
2021-03-24 03:11:48 -0500

    return 400 http status on invalid input

--- back/main.py
+++ back/main.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask import request
+import torre.skill_differences as skills
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/skillDifferences', methods=['POST'])
+def skill_differences():
+    if request.method == 'POST':
+        input_data = request.json
+
+        valid, err_msg = skills.validate_input(input_data)
+
+        if not valid:
+            return err_msg, 400
+        
+        return skills.get_skill_differences(input_data)
+        
