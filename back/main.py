b925c605fa78ea8595f9cd1426a61b2f95abc044
Mauricio Neira <m.neira10@uniandes.edu.co>
2021-03-24 04:08:28 -0500

    disable CORS

--- back/main.py
+++ back/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 import torre.skill_differences as skills
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/skillDifferences', methods=['POST'])
 def skill_differences():
